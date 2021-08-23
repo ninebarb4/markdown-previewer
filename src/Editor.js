87d9dc639d77fd60241b35c55773722a0c5d0418
James Burgess <james.burgess4@gmail.com>
2021-08-23 14:44:51 +0100

    give min/max dimensions to (editor) text area

--- src/Editor.js
+++ src/Editor.js
@@ -3,8 +3,8 @@ import './Editor.css';
 
 export default function Editor() {
     return (
-        <div >
-            <textarea className="text-areas" id="editor"  id="" cols="30" rows="10">
+        <div className="text-container">
+            <textarea className="text-areas" id="editor">
 
             </textarea>
         </div>
