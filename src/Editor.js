4cf98ed7c24700e349203029b79b097751f20cde
James Burgess <james.burgess4@gmail.com>
2021-08-23 12:28:54 +0100

    add editor component with starter text area

---
+++ src/Editor.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Editor() {
+    return (
+        <div>
+            <textarea name="editor-text" id="" cols="30" rows="10"></textarea>
+        </div>
+    )
+}
