7e9573d6adc445ca7a20e1632128a60319fc042f
James Burgess <james.burgess4@gmail.com>
2021-08-25 18:52:06 +0100

    add 'marked' cdn script to html body and update previewer id

--- src/Previewer.js
+++ src/Previewer.js
@@ -5,7 +5,7 @@ export default function Previewer(props) {
     return (
         <div className="text-container">
             {/* need to use value attribute in text area to update rather than just inserting props.text netween textarea element tags*/}
-            <textarea className="text-areas" id="editor" value={props.text}>
+            <textarea className="text-areas" id="preview" value={props.text}>
             
             </textarea>
         </div>
