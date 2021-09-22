649f9bf3d8157f2f3c69f9c7c71be025ef34e3f7
James Burgess <james.burgess4@gmail.com>
2021-09-22 21:41:19 +0100

    add auto resizing of tinput textarea

--- src/Editor.js
+++ src/Editor.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import './TextAreas.css';
+import { useState, useEffect, useRef } from 'react';
 
 export default function Editor(props) {
-    function handleChange(e){
-        console.log(e.target.value);
-        props.setText(e.target.value);
+    //give a default height to editor text area
+    const defaultHeight = "40px";
+    //useRef hook used to grab text area element
+    const textAreaRef = useRef();
+    
+    function handleChange(element){
+        //set "text" held in parent state
+        props.setText(element.target.value);
     }
+
+    //when text updated on change use effect will alter scrollHeight of element
+   useEffect(() => {
+       //first set height back to default height incase user deletes content
+       //if they did this scroll height would remain the same but would have large empty text area
+        textAreaRef.current.style.height = defaultHeight;
+        const scrollHeight = textAreaRef.current.scrollHeight;
+        textAreaRef.current.style.height = scrollHeight + "px";
+    }, [props.text])
+
+   
+
     return (
         <div className="text-container">
-            <textarea className="text-areas" id="editor" onChange={handleChange}>
+            <textarea 
+            ref={textAreaRef}
+            className="text-areas" 
+            id="editor" 
+            value={props.text}
+            style={{paddingTop: "0px", paddingBottom: "0px"}} 
+            onChange={handleChange}
+            >
 
             </textarea>
         </div>
